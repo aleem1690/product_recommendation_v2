f103737b812105a0604c410fe4087f94e575d709
aleem1690 <63804835+aleem1690@users.noreply.github.com>
2023-08-28 21:56:23 +0530

    Update product_recommendation_frontend.py

--- product_recommendation_frontend.py
+++ product_recommendation_frontend.py
@@ -34,7 +34,7 @@ def main():
         st.write("We would love to hear from you!")
         audio_bytes = audio_recorder()
         product_needs_voice = st.audio(audio_bytes, format="audio/wav")
-        st.write(type(products_needs_voice)
+        st.write(type(products_needs_voice))
         # user_input_text = model.transcribe(product_needs_voice)
 
         # with sr.AudioFile(product_needs_voice) as source:
