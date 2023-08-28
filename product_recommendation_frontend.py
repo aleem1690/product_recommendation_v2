9646cf5338151d5d1f4096b5d72f6f3489a06b76
aleem1690 <63804835+aleem1690@users.noreply.github.com>
2023-08-28 23:59:52 +0530

    Update product_recommendation_frontend.py

--- product_recommendation_frontend.py
+++ product_recommendation_frontend.py
@@ -47,7 +47,7 @@ def main():
 
             st.write(wav_file.name)
 
-            audio_tbt = whisper.load_audio(wav_file)
+            audio_tbt = whisper.load_audio(wav_file.name)
         
         # typ = type(audio_bytes)
         # st.write(typ)
